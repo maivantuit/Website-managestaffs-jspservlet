4ec49614ffc5e21e87f54214f745bbba552bdc06
maivantuit <31197881+maivantuit@users.noreply.github.com>
2017-12-30 10:29:39 +0700

    Update Staff.java

--- src/java/modelclasses/Staff.java
+++ src/java/modelclasses/Staff.java
@@ -86,6 +86,7 @@ public class Staff {
                 System.out.println(ten);
                 dob = rs.getString("Dob");
                 sex = rs.getString("Sex");
+                // git2
                 System.out.println(sex);
                 CMND = rs.getString("CMND");
                 pW = rs.getString("PW");
